9dba5a31e72566bbbed2bb932efceb0e4e94f7b4
tewereus16 <tewereus16@gmail.com>
2024-05-19 01:46:49 +0800

    Modified userModel
    Added preference for the dark or default mode

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -66,6 +66,11 @@ const userSchema = mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    preference: {
+      type: String,
+      enum: ["default", "dark"],
+      default: "default",
+    },
     address: {
       type: String,
     },
