c23caf251848cf9653c359f763d9c5702be67d08
tewereus <tewereus12312@gmail.com>
2024-06-06 20:14:50 +0300

    Started Product Slice
    started product slice for createProduct

--- admin/src/features/products/productSlice.js
+++ admin/src/features/products/productSlice.js
@@ -1,16 +1,55 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, asyncThunkCreator } from "@reduxjs/toolkit";
+import productService from "./productService";
 
 const initialState = {
+  products: [],
+  totalProducts: 0,
   isLoading: false,
   isSuccess: false,
   isError: false,
   message: "",
 };
 
+const createProduct = asyncThunkCreator(
+  "product/create-product",
+  async (data, thunkAPI) => {
+    try {
+      return await productService.createProduct(data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
 export const productSlice = createSlice({
   name: "products",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createProduct.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createProduct.fulfilled, (state) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.message = "";
+        state.createdProduct = action.payload;
+        if (state.isSuccess === true) {
+          toast.success("Product added Successfully");
+        }
+      })
+      .addCase(createProduct.rejected, (state) => {
+        state.isLoading = false;
+        state.isSuccess = false;
+        state.isError = true;
+        state.message = action.error;
+        if (state.isError === true) {
+          toast.error(action.payload.response.data.message);
+        }
+      });
+  },
 });
 
 export default productSlice.reducer;
--- admin/src/store/store.js
+++ admin/src/store/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import usersReducer from "../features/users/userSlice";
-import productsReducer from "../features/users/userSlice";
+import productReducer from "../features/products/productSlice";
 
 export const store = configureStore({
   reducer: {
