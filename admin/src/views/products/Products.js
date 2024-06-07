ed152aa56194478d84c71981278a959f908445ad
tewereus <tewereus12312@gmail.com>
2024-06-07 22:46:57 +0300

    Modified Products and added AddProduct

--- admin/src/views/products/Products.js
+++ admin/src/views/products/Products.js
@@ -1,63 +1,24 @@
-import React, { useState } from "react";
-import { createProduct } from "../../features/products/productSlice";
-import { useDispatch } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { getAllProducts } from "../../features/products/productSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const Products = () => {
   const dispatch = useDispatch();
-  const [productState, setProductState] = useState({
-    title: "",
-    description: "",
-    basePrice: 0,
-  });
-
-  const handleChange = (e) => {
-    setProductState({
-      ...productState,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const data = {
-      title: productState.title,
-      description: productState.description,
-      basePrice: productState.basePrice,
-    };
-    dispatch(createProduct(data));
-  };
-
+  useEffect(() => {
+    dispatch(getAllProducts());
+  }, []);
+  const { products } = useSelector((state) => state.products);
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Title:
-        <input
-          type="text"
-          value={productState.title}
-          name="title"
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Description:
-        <textarea
-          value={productState.description}
-          name="description"
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Price:
-        <input
-          type="number"
-          value={productState.basePrice}
-          name="basePrice"
-          onChange={handleChange}
-        />
-      </label>
-      <button type="submit">Create Product</button>
-    </form>
+    <div>
+      {products.map((product) => {
+        return (
+          <div key={product._id}>
+            <p>{product.title}</p>
+            <p>{product.basePrice}</p>
+          </div>
+        );
+      })}
+    </div>
   );
 };
-
 export default Products;
