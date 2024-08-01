9d1521a3a2774b1710c8060ae52110c8f49c3ed2
tewereus <tewereus12312@gmail.com>
2024-08-01 16:00:51 +0300

    Finished color service

---
+++ admin/src/features/color/colorService.js
@@ -0,0 +1,37 @@
+import { base_url, config } from "../../api/axiosConfig";
+import axios from "axios";
+
+const addColor = async (data) => {
+    const response = await axios.post(`${base_url}/colors/create-color`, data)
+    return response.data
+}
+
+const getAllColors = async () => {
+    const response = await axios.get(`${base_url}/colors/all-colors`)
+    return response.data
+}
+
+const updateColor = async (id) => {
+    const response = await axios.put(`${base_url}/colors/${id}`)
+    return response.data
+}
+
+const deleteColor = async () => {
+    const response = await axios.delete(`${base_url}/colors/delete/${id}`)
+    return response.data
+}
+
+const deleteAllColors = async () => {
+    const response = await axios.delete(`${base_url}/colors/delete-all`)
+    return response.data
+}
+
+const colorService = {
+    addColor,
+    getAllColors,
+    updateColor,
+    deleteColor,
+    deleteAllColors
+}
+
+export default colorService
