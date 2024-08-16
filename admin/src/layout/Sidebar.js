e1029eaae43e3de100f51887c96381535f441099
tewereus <tewereus12312@gmail.com>
2024-08-16 10:48:25 +0300

    added logout functionality for admin

--- admin/src/layout/Sidebar.js
+++ admin/src/layout/Sidebar.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
+  const navigate = useNavigate();
   const [isUsersOpen, setIsUsersOpen] = useState(false);
   const [isImagesOpen, setIsImagesOpen] = useState(false);
   const [isProductsOpen, setIsProductsOpen] = useState(false);
+
+  const handleLogout = () => {
+    localStorage.clear();
+    // dispatch(user_reset())
+    navigate("/");
+    window.location.reload();
+  };
 
   return (
     <div>
@@ -14,39 +22,63 @@ const Sidebar = () => {
         </li>
         <li>
           <div>
-            <button onClick={() => {
-              setIsUsersOpen(!isUsersOpen)
-              setIsImagesOpen(false)
-              setIsProductsOpen(false)
-            }}
-              
-              style={{ cursor: 'pointer', background: 'none', border: 'none', padding: '0', margin: '0' }}>
+            <button
+              onClick={() => {
+                setIsUsersOpen(!isUsersOpen);
+                setIsImagesOpen(false);
+                setIsProductsOpen(false);
+              }}
+              style={{
+                cursor: "pointer",
+                background: "none",
+                border: "none",
+                padding: "0",
+                margin: "0",
+              }}
+            >
               Users
             </button>
             {isUsersOpen && (
               <ul className="bg-[#999] pl-[10px]">
-                <li><Link to="users">Users</Link></li>
-                <li><Link to="managers">Managers</Link></li>
-                <li><Link to="printers">Printers</Link> </li>
+                <li>
+                  <Link to="users">Users</Link>
+                </li>
+                <li>
+                  <Link to="managers">Managers</Link>
+                </li>
+                <li>
+                  <Link to="printers">Printers</Link>{" "}
+                </li>
               </ul>
             )}
           </div>
         </li>
         <li>
           <div>
-            <button onClick={() => {
-              setIsProductsOpen(!isProductsOpen)
-              setIsUsersOpen(false)
-              setIsImagesOpen(false)
-            }}
-              
-              style={{ cursor: 'pointer', background: 'none', border: 'none', padding: '0', margin: '0' }}>
+            <button
+              onClick={() => {
+                setIsProductsOpen(!isProductsOpen);
+                setIsUsersOpen(false);
+                setIsImagesOpen(false);
+              }}
+              style={{
+                cursor: "pointer",
+                background: "none",
+                border: "none",
+                padding: "0",
+                margin: "0",
+              }}
+            >
               Products
             </button>
             {isProductsOpen && (
               <ul className="bg-[#999] pl-[10px]">
-                <li><Link to="products">Products</Link></li>
-                <li><Link to="product-types">Product Types</Link></li>
+                <li>
+                  <Link to="products">Products</Link>
+                </li>
+                <li>
+                  <Link to="product-types">Product Types</Link>
+                </li>
               </ul>
             )}
           </div>
@@ -58,22 +90,36 @@ const Sidebar = () => {
           <div>
             <button
               onClick={() => {
-                setIsImagesOpen(!isImagesOpen)
-                setIsUsersOpen(false)
-                setIsProductsOpen(false)
+                setIsImagesOpen(!isImagesOpen);
+                setIsUsersOpen(false);
+                setIsProductsOpen(false);
               }}
-              style={{ cursor: 'pointer', background: 'none', border: 'none', padding: '0', margin: '0' }}>
+              style={{
+                cursor: "pointer",
+                background: "none",
+                border: "none",
+                padding: "0",
+                margin: "0",
+              }}
+            >
               Images
             </button>
             {isImagesOpen && (
               <ul className="bg-[#999] pl-[10px]">
-                <li><Link to="images">Images</Link></li>
-                <li><Link to="image-types">Image Types</Link></li>
-                <li><Link to="image-categories">Image Categories</Link> </li>
+                <li>
+                  <Link to="images">Images</Link>
+                </li>
+                <li>
+                  <Link to="image-types">Image Types</Link>
+                </li>
+                <li>
+                  <Link to="image-categories">Image Categories</Link>{" "}
+                </li>
               </ul>
             )}
           </div>
         </li>
+        <li onClick={handleLogout}>logout</li>
       </ul>
     </div>
   );
--- server/models/users/managerModel.js
+++ server/models/users/managerModel.js
@@ -1,72 +1,86 @@
-const mongoose = require('mongoose')
-const crypto = require("crypto")
-const bcrypt = require("bcryptjs")
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+const bcrypt = require("bcryptjs");
 
-const managerSchema = mongoose.Schema({
+const managerSchema = mongoose.Schema(
+  {
     unique_id: String,
     fullname: {
-        type: String,
-        // required: [true, "full name is required"],
-        set: (v) => v.trim().replace(/\s+/g, " "),
-      },
+      type: String,
+      // required: [true, "full name is required"],
+      set: (v) => v.trim().replace(/\s+/g, " "),
+    },
     email: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     mobile: {
-        type: String,
-        required: [true, "mobile is required"],
-        unique: [true, "mobile number already registered"],
-        validate: {
-          validator: function (v) {
-            return /^\d{9}$/.test(v); // Validate that mobile contains exactly 9 digits
-          },
-          message: (props) => `${props.value} is not a valid mobile number!`,
+      type: String,
+      required: [true, "mobile is required"],
+      unique: [true, "mobile number already registered"],
+      validate: {
+        validator: function (v) {
+          return /^\d{9}$/.test(v); // Validate that mobile contains exactly 9 digits
         },
+        message: (props) => `${props.value} is not a valid mobile number!`,
       },
+    },
     password: {
-        type: String,
-        // required: true,
-        select: false
+      type: String,
+      // required: true,
+      select: false,
     },
     role: {
-        type: String,
-        default: 'manager'
+      type: String,
+      default: "manager",
     },
-    status: { // this is for the managers to make it inactive incase of an emergency
-        type: String,
-        required: true,
-        enum: ["active", "inactive"],
-        default: 'inactive'
+    status: {
+      // this is for the managers to make it inactive incase of an emergency
+      type: String,
+      required: true,
+      enum: ["active", "inactive"],
+      default: "inactive",
     },
-    main_status: { // this is for the admin to make them active after verifying the manager info 
-        type: String,
-        require: true,
-        enum: ["active", "inactive", "waiting", "unavailable"], // unavailable if the manager is not working anymore(change/ fired/ retired)
-        default: "inactive"
+    main_status: {
+      // this is for the admin to make them active after verifying the manager info
+      type: String,
+      require: true,
+      enum: ["active", "inactive", "waiting", "unavailable"], // unavailable if the manager is not working anymore(change/ fired/ retired)
+      default: "inactive",
     },
-    printers: [{
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Printer"
-    }],
-    payment: [{
-      bankName: String,
-      bankAccount: String //hash this if needed
-    }],
+    printers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Printer",
+      },
+    ],
+    payment: [
+      {
+        bankName: String,
+        bankAccount: String, //hash this if needed
+      },
+    ],
     profile: {
-        type: String,
-        default: ''
+      type: String,
+      default: "",
     },
     address: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "Address"
+      ref: "Address",
     },
     // may be remove this
     shopInfo: {
-        type: Object,
-        default: {}
-    }
-}, { timestamps: true })
+      type: Object,
+      default: {},
+    },
+    sold: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
+  },
+  { timestamps: true }
+);
 
 managerSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
@@ -86,11 +100,11 @@ managerSchema.methods.isPasswordMatched = async function (enteredPassword) {
 };
 
 managerSchema.methods.createManagerToken = async function () {
-    const token = crypto.randomBytes(3).toString("hex");
-    this.unique_id = token
+  const token = crypto.randomBytes(3).toString("hex");
+  this.unique_id = token;
 
-    console.log("token: ", token)
-    return token;
-  };
+  console.log("token: ", token);
+  return token;
+};
 
-module.exports = mongoose.model('Manager', managerSchema)
+module.exports = mongoose.model("Manager", managerSchema);
